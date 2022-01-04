fed3e54cfbabacfc48c370c5a9e763ddeb8282d7
Lorenzo Signoretti <lory4846@hotmail.it>
2022-01-04 19:54:13 +0100

    responsive categories

--- template/categorie-in.php
+++ template/categorie-in.php
@@ -1,8 +1,8 @@
 <div class="m-5">
 </div>
-<div class="m-5 d-flex justify-content-center overflow-auto">
+<div class="row m-5 d-flex justify-content-center">
     <?php foreach($templateParams["categorie"] as $categoria): ?>
-        <div class="card m-3 w-25" style="width: 18rem;">
+        <div class="card m-3" style="width: 18rem;">
             <img src="./resources/img/<?php echo $categoria['img']?>" class="card-img-top" alt="<?php echo $categoria['nomeCategoria'] ?>"/>
             <div class="card-body">
                 <h5 class="card-title"><?php echo $categoria['nomeCategoria']; ?></h5>
