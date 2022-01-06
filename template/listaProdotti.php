904422d4b3853934ab2797eb11b0997a1a723aa3
Lorenzo Signoretti <lory4846@hotmail.it>
2022-01-06 23:14:40 +0100

    Create provisions for products

--- template/listaProdotti.php
+++ template/listaProdotti.php
@@ -1,49 +1,47 @@
 <div>
-                <h1>Risultati per "xxx"</h1>
+    <h1>Risultati per "xxx"</h1>
+</div>
+
+<div class="album py-5">
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            
+            <div class="col d-none d-md-flex">
+                <div class="card shadow-sm"> 
+                    <img src="./resources/img/Logo.png" class="card-img-top" alt=""/>
+                    <div class="card-body">
+                        <h5 class="card-title">Nome Prodotto</h5>
+                        <p class="card-text">Descrizione prodotto</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Aggiugni al carrello</button>
+                            <small class="text-muted">€ 0,00</small>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <table>
-                  <tr>
-                      <td>
-                          <a href="#">
-                              <img src="./resources/img/logo-small.png" alt="Articolo 1"/>
-                          </a>
-                      </td>
-                      <td>
-                          <h2>Nome prodotto</h2>
-                          <p>Nome venditore</p>
-                      </td>
-                      <td>
-                          <p>Prezzo</p>
-                      </td>
-                  </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <img src="./resources/img/logo-small.png" alt="Articolo 2"/>
-                            </a>
-                        </td>
-                        <td>
-                            <h2>Nome prodotto</h2>
-                            <p>Nome venditore</p>
-                        </td>
-                        <td>
-                            <p>Prezzo</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <img src="./resources/img/logo-small.png" alt="Articolo 3"/>
-                            </a>
-                        </td>
-                        <td>
-                            <h2>Nome prodotto</h2>
-                            <p>Nome venditore</p>
-                        </td>
-                        <td>
-                            <p>Prezzo</p>
-                        </td>
-                    </tr>
-                </table>
+
+            <div class="card mb-3 d-md-none" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-4">
+                        <img src="./resources/img/Logo.png" class="img-fluid rounded-start" alt=""/>
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Nome Prodotto</h5>
+                            <p class="card-text">Descrizione prodotto</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Aggiugni al carrello</button>
+                                <small class="text-muted">€ 0,00</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+        </div>
+      
+    </div>
+</div>
+
