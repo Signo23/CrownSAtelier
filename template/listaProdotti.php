20c6429adbcb12dbefd6a5472bbcbe8bc73f90a6
Lorenzo Signoretti <lory4846@hotmail.it>
2022-01-08 15:06:26 +0100

    Delete 'add to cart''s button and set href to product.php

--- template/listaProdotti.php
+++ template/listaProdotti.php
@@ -1,4 +1,4 @@
-<div>
+<div class="m-5">
     <h1>Risultati per "xxx"</h1>
 </div>
 
@@ -7,21 +7,7 @@
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             
-            <div class="col d-none d-md-flex">
-                <div class="card shadow-sm"> 
-                    <img src="./resources/img/Logo.png" class="card-img-top" alt=""/>
-                    <div class="card-body">
-                        <h5 class="card-title">Nome Prodotto</h5>
-                        <p class="card-text">Descrizione prodotto</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Aggiugni al carrello</button>
-                            <small class="text-muted">€ 0,00</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-3 d-md-none" style="max-width: 540px;">
+        <div class="card mb-3 d-md-none" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-4">
                         <img src="./resources/img/Logo.png" class="img-fluid rounded-start" alt=""/>
@@ -30,8 +16,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Nome Prodotto</h5>
                             <p class="card-text">Descrizione prodotto</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Aggiugni al carrello</button>
+                            <a href="product.php" class="stretched-link"></a>
+                            <div class="d-flex justify-content-end align-items-center">
                                 <small class="text-muted">€ 0,00</small>
                             </div>
                         </div>
@@ -39,6 +25,19 @@
                 </div>
             </div>
 
+            <div class="col d-none d-md-flex">
+                <div class="card shadow-sm"> 
+                    <img src="./resources/img/Logo.png" class="card-img-top" alt=""/>
+                    <div class="card-body">
+                        <h5 class="card-title">Nome Prodotto</h5>
+                        <p class="card-text">Descrizione prodotto</p>
+                        <a href="product.php" class="stretched-link"></a>
+                        <div class="d-flex justify-content-end align-items-center">
+                            <small class="text-muted">€ 0,00</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
       
