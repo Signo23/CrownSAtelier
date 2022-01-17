4c12e66a407c9c7e5b9e88cce0b4db88e217df0e
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-01-18 00:28:26 +0100

    Create login.php

---
+++ template/login.php
@@ -0,0 +1,23 @@
+<form>
+  <img src="./resources/img/Logo.png" alt="" style="width: 50px;"/>
+  <h1 class="h3 mb-3 fw-normal">Accedi</h1>
+
+  <div class="form-floating">
+    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+    <label for="floatingInput">Email</label>
+  </div>
+
+  <div class="form-floating">
+    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+    <label for="floatingPassword">Password</label>
+  </div>
+
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Ricordami
+    </label>
+  </div>
+
+  <button class="w-100 btn btn-lg btn-primary" type="submit">Accedi</button>
+  <p class="mt-5 mb-3 text-muted">&copy; Crown's Atelier, 2022</p>
+</form>
