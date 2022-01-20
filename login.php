20c303aff303e7f1f19aa9d3db1aa211a4dc5f51
Lorenzo Signoretti <lory4846@hotmail.it>
2022-01-20 18:25:32 +0100

    Create the access to login

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'start.php';
+
+//Base Template
+$templateParams["titolo"] = "Crown's Atelier - Login";
+$templateParams["nome"] = "login.php";
+$templateParmas["css"] = "login.css";
+//Categorie Template
+
+
+require 'template/no-nav-base.php';
+?>
