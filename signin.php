d810516b2746045782537efd03a53f8b2f573b7f
Lorenzo Signoretti <lory4846@hotmail.it>
2022-01-20 18:25:49 +0100

    Create the access to signin

---
+++ signin.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'start.php';
+
+//Base Template
+$templateParams["titolo"] = "Crown's Atelier - Signin";
+$templateParams["nome"] = "signin-cliente.php";
+//$templateParmas["css"] = "signin.css";
+//Categorie Template
+
+
+require 'template/no-nav-base.php';
+?>
