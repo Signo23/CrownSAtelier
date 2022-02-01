86c4c62f597b9a3a46c43d5f71c1faba552b7dc1
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-02-01 17:10:23 +0100

    Create signin-choose.php and signin-choose.css

---
+++ template/signin-choose.php
@@ -0,0 +1,38 @@
+<main>
+
+<div class="m-5">
+    <h1>Registrazione</h1>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal">Fornitore</h4>
+            </div>
+            <div class="card-body">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Puoi aggiungere i tuoi prodotti al catalogo di Crown's Atelier</li>
+                    <li>Per ogni acquisto di un tuo prodotto effettuato da un utente riceverai una notifica</li>
+                </ul>
+                <a href="signin-fornitore.php" class="btn btn-success stretched-link">Registrati</a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal">Cliente</h4>
+            </div>
+            <div class="card-body">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Crown's Atelier ti permette di acquistare ci&ograve; che pi&ugrave; fa per te tramite un'ampia scelta di prodotti divisi in categorie</li>
+                    <li>Hai la possibilit&agrave; di acquistare prodotti scegliendo tra pi&ugrave; fornitori</li>
+                </ul>
+                <a href="signin-cliente.php" class="btn btn-success stretched-link">Registrati</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</main>
