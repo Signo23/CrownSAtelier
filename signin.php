a844c2317f10ff7e07cbde5554f841040a5b32d3
Lorenzo Signoretti <lory4846@hotmail.it>
2022-02-01 18:01:50 +0100

    Minor fix

--- signin.php
+++ signin.php
@@ -3,9 +3,14 @@ require_once 'start.php';
 
 //Base Template
 $templateParams["titolo"] = "Crown's Atelier - Signin";
-$templateParams["nome"] = "signin-cliente.php";
-$templateParmas["css"] = "signin.css";
-//Categorie Template
+if(isset($_GET['id'])){
+    $templateParams["nome"] = "signin-" .$_GET['id'] .".php";
+    $templateParmas["css"] = "signin.css";
+
+} else {
+    $templateParams["nome"] = "signin-choose.php";
+
+}
 
 
 require 'template/no-nav-base.php';
--- template/signin-choose.php
+++ template/signin-choose.php
@@ -1,35 +1,35 @@
-<main>
+<main class="pt-md-5">
 
-<div class="m-5">
+<div class="m-5 pt-md-5">
     <h1>Registrazione</h1>
 </div>
 
-<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-    <div class="col">
+<div class="row row-cols-1 row-cols-md-2 mb-3 text-center mx-0 px-2 mt-5">
+    <div class="col mt-5">
         <div class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal">Fornitore</h4>
             </div>
             <div class="card-body">
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>Puoi aggiungere i tuoi prodotti al catalogo di Crown's Atelier</li>
-                    <li>Per ogni acquisto di un tuo prodotto effettuato da un utente riceverai una notifica</li>
+                    <li>Puoi aggiungere i tuoi prodotti al catalogo di Crown's Atelier.</li>
+                    <li>Ogni volta che qualcuno acquister&agrave; un tuo prdotto da un utente riceverai una notifica</li>
                 </ul>
-                <a href="./signin-fornitore.php" class="btn btn-success stretched-link">Registrati</a>
+                <a href="signin.php?id=fornitore" class="btn btn-success stretched-link">Registrati</a>
             </div>
         </div>
     </div>
-    <div class="col">
+    <div class="col mt-5">
         <div class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal">Cliente</h4>
             </div>
             <div class="card-body">
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>Crown's Atelier ti permette di acquistare ci&ograve; che pi&ugrave; fa per te tramite un'ampia scelta di prodotti divisi in categorie</li>
-                    <li>Hai la possibilit&agrave; di acquistare prodotti scegliendo tra pi&ugrave; fornitori</li>
+                    <li>Puoi acquistare ci&ograve; che pi&ugrave; fa per te tramite un'ampia scelta di prodotti.</li>
+                    <li>Hai la possibilit&agrave; di acquistare scegliendo tra pi&ugrave; fornitori</li>
                 </ul>
-                <a href="./signin-cliente.php" class="btn btn-success stretched-link">Registrati</a>
+                <a href="signin.php?id=cliente" class="btn btn-success stretched-link">Registrati</a>
             </div>
         </div>
     </div>
