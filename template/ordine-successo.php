5a4b03652f3193abb79a99055b6633f6ccb28f88
Giulia Montini <giulia.montini2@studio.unibo.it>
2022-02-02 16:00:28 +0100

    Create ordine-successo.php

---
+++ template/ordine-successo.php
@@ -0,0 +1,10 @@
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="display-5 fw-bold">Ordine effettuato con successo!</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Riceverai il tuo ordine presso il Campus di Cesena.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <a href="./index.php" class="btn btn-success stretched-link">Continua lo shopping</a>
+        </div>
+    </div>
+</div>
