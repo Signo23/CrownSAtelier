b833c399eb8156d8c7253ea84bdc332fc00e62dc
Lorenzo Signoretti <lory4846@hotmail.it>
2022-02-03 18:52:07 +0100

    Import use of signin-choose and implement signin fornitore

--- signin.php
+++ signin.php
@@ -3,15 +3,30 @@ require_once 'start.php';
 
 //Base Template
 $templateParams["titolo"] = "Crown's Atelier - Signin";
-if(isset($_GET['id'])){
-    $templateParams["nome"] = "signin-" .$_GET['id'] .".php";
-    $templateParmas["css"] = "signin.css";
 
-} else {
-    $templateParams["nome"] = "signin-choose.php";
+if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["telefono"]) && isset($_POST["nomeNegozio"]) && isset($_POST["indirizzo"])){
+    $dbh->signinFornitore($_POST["email"], $_POST["password"], $_POST["telefono"], $_POST["nomeNegozio"], $_POST["indirizzo"]);
+    $login_res = $dbh->checkLogin($_POST["email"], $_POST["password"]);
+    if(count($login_res)==0) {
+        $templateParmas["errore"] = 'Errore! Email o password errati';
+    }
+    else{
+        registerLoggedUser($login_res[0]);
+    }
+    require 'index.php';
+} elseif(isset($_GET['id'])){
+        $templateParams["nome"] = "signin-" .$_GET['id'] .".php";
+        $templateParmas["css"] = "signin.css";
+        require 'template/no-nav-base.php';
 
-}
+    } 
+     else {
+        $templateParams["nome"] = "signin-choose.php";
+        require 'template/no-nav-base.php';
+
+    }
 
 
-require 'template/no-nav-base.php';
+
+
 ?>
