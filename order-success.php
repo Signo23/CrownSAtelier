aeed87cdb76563f2c51c2cd781ce096fee465ef5
Lorenzo Signoretti <lory4846@hotmail.it>
2022-02-12 05:10:06 +0100

    Add animation

---
+++ order-success.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'start.php';
+
+    $templateParams["titolo"] = "Crown's Atelier - Login";
+    $templateParams["nome"] = "template/ordine-successo.php";
+    //$templateParmas["css"] = "login.css";
+    require 'template/no-nav-base.php';
+
+
+?>
--- template/ordine-successo.php
+++ template/ordine-successo.php
@@ -3,8 +3,12 @@
     <h1 class="display-5 fw-bold">Ordine effettuato con successo!</h1>
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">Riceverai il tuo ordine presso il Campus di Cesena.</p>
+        <div class="d-flex justify-content-center">
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <lottie-player src="https://assets7.lottiefiles.com/temp/lf20_fxHtqF.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+        </div>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a href="./index.php" class="btn btn-success stretched-link">Continua lo shopping</a>
+            <a href="./index.php" class="btn btn-success">Continua lo shopping</a>
         </div>
     </div>
 </div>
