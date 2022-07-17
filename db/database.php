267ce11194ae2ef0e91f51e7849a471afe48be3e
Lorenzo Signoretti <lory4846@hotmail.it>
2022-07-17 12:45:37 +0200

    Write base function to develop

--- db/database.php
+++ db/database.php
@@ -161,63 +161,18 @@ class DatabaseHelper{
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 
-    /*public function getRandomPosts($n){
-        $stmt = $this->db->prepare("SELECT idarticolo, titoloarticolo, imgarticolo FROM articolo ORDER BY RAND() LIMIT ?");
-        $stmt->bind_param('i',$n);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
+    public function placeOrder($userPkid){
     }
 
-    
-
-    public function getCategoryById($idcategory){
-        $stmt = $this->db->prepare("SELECT nomecategoria FROM categoria WHERE idcategoria=?");
-        $stmt->bind_param('i',$idcategory);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getPosts($n=-1){
-        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore WHERE autore=idautore ORDER BY dataarticolo DESC";
-        if($n > 0){
-            $query .= " LIMIT ?";
-        }
-        $stmt = $this->db->prepare($query);
-        if($n > 0){
-            $stmt->bind_param('i',$n);
-        }
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getPostById($id){
-        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, testoarticolo, dataarticolo, nome FROM articolo, autore WHERE idarticolo=? AND autore=idautore";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    
-
-    public function getAuthors(){
-        $query = "SELECT username, nome, GROUP_CONCAT(DISTINCT nomecategoria) as argomenti FROM categoria, articolo, autore, articolo_ha_categoria WHERE idarticolo=articolo AND categoria=idcategoria AND autore=idautore AND attivo=1 GROUP BY username, nome";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-	
-	
-    }*/
+    //############################################################################
+    //############################################################################
+    //##                                                                        ##
+    //##                           NOTIFICATION                                 ##
+    //##                                                                        ##
+    //############################################################################
+    //############################################################################
+    public function notifyOrderShipped(){};
+    public function notifyAddItemInOrder(){};
+    public function numberOfNotication(){};
 }
 ?>
