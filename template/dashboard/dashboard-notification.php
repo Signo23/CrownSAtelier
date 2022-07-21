ba628987d466f367ce76bb9ee19851607f2719c2
Lorenzo Signoretti <lory4846@hotmail.it>
2022-07-21 18:39:43 +0200

    Add animation for no order and no notification

---
+++ template/dashboard/dashboard-notification.php
@@ -0,0 +1,7 @@
+<?php if(!isset($templateParams['notication']) ||  count($templateParams['notication'])!=0 ): ?>
+    <div class="d-flex">
+        <h3 class="m-3">Nessuna notifica<h3>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_c5woctcf.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+    </div>
+<?php endif; ?>
