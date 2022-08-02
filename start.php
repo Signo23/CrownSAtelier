a5bb211458f03c324189ff0483f01412b0c62f5a
Lorenzo Signoretti <lory4846@hotmail.it>
2022-08-02 16:47:20 +0200

    Define upload dir

--- start.php
+++ start.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//define("UPLOAD_DIR", "./upload/");
+define("UPLOAD_DIR", "./upload/");
 require_once("utils/functions.php");
 require_once("db/database.php");
 $dbh = new DatabaseHelper("127.0.0.1", "root", "root", "my_crownsatelier", 3306);
