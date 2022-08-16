22e21ed34f7f948afea4d1ae5c02d3d634db3063
Lorenzo Signoretti <lory4846@hotmail.it>
2022-08-16 20:58:51 +0200

    Responsive form for upload

--- template/dashboard/product/product-form.php
+++ template/dashboard/product/product-form.php
@@ -1,11 +1,11 @@
 <form class="row g-3 needs-validation m-2">
     <div class="mb-3 col-12">
         <label for="nome" class="form-label">Nome prodotto</label>
-        <input type="text" class="form-control" id="nome" required>
+        <input type="text" class="form-control" id="nome" <?php if($_GET['form']==2 || $_GET['form']==3) {echo 'disabled';}?> required>
     </div>
     <div class="mb-3 me-4 col-12">
         <label for="descrizione" class="form-label">Descrizione</label>
-        <textarea class="form-control" id="descrizione" rows="3" required></textarea>
+        <textarea class="form-control" id="descrizione" rows="3" <?php if($_GET['form']==2 || $_GET['form']==3) {echo 'disabled';}?>  required></textarea>
     </div>
     <div class="mb-3 col-12 col-md-4">
         <label for="price" class="form-label" aria-label="Amount">Prezzo</label>
@@ -18,17 +18,17 @@
 
     <div class="mb-3 col-12 col-md-4">
         <label for="tag" class="form-label">Categoria</label>
-        <select class="form-select" aria-label="Default select example" id="tag" required>
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <select class="form-select" aria-label="Default select example" id="tag" <?php if($_GET['form']==2 || $_GET['form']==3) {echo 'disabled';}?>  required>
+            <option selected>Seleziona una categoria</option>
+            <?php foreach($dbh->getCategories() as $categoria): ?>
+                <option value="<?php echo $categoria['idCategoria'];?>"><?php echo $categoria['nomeCategoria']; ?></option>
+            <?php endforeach;?>
         </select>
     </div>
 
-    <div class="mb-3 col-12">
-        <label for="formFile" class="form-label">Default file input example</label>
-        <input class="form-control" type="file" id="formFile" required>
+    <div class="mb-3 col-12 <?php if($_GET['form']!=1) {echo 'd-none';}?>">
+        <label for="formFile" class="form-label">Foto prodotto</label>
+        <input class="form-control" type="file" id="formFile" <?php if($_GET['form']==1) {echo 'required';}?>>
     </div>
 
     <div class="mb-3 col-12 d-flex justify-content-end align-items-center">
